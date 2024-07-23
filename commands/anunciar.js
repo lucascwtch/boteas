49c946d7a4f8bf6e3b6afbe6a16d7c39a548a782
Lucas Almeida Costa <lucas.cwtch@gmail.com>
2024-07-23 19:33:10 -0300

    Update anunciar.js

--- commands/anunciar.js
+++ commands/anunciar.js
@@ -9,7 +9,7 @@ module.exports = {
 
     if (!message.member.permissions.has("MANAGE_SERVER")) return message.reply(`${message.author} **Voce não possui permissão para esse comando.**`); 
 
-    if (!eas_say) return message.channel.send(`❌ | ${message.author} Você precisa escrever algo para eu falar!`);
+    if (!eas_say) return message.channel.send(`❌ | ${message.author} Você precisa escrever algo para que eu possa falar!`);
 
     message.channel.send(`${eas_say}
 
